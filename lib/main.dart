d6c80e22149cb302856dde2be0c718bc80ffe7c3
nickcatalin <florin.nicola@stud.acs.upb.ro>
2022-03-26 15:45:16 +0200

    bjhihj

--- lib/main.dart
+++ lib/main.dart
@@ -97,7 +97,7 @@ class MainPage extends StatelessWidget {
                         decoration: InputDecoration(
                             icon: const Icon(
                               Icons.email,
-                              color: Colors.blue,
+                              color: Colors.black,
                             ),
                             focusedBorder: UnderlineInputBorder(
                                 borderSide:
@@ -111,7 +111,7 @@ class MainPage extends StatelessWidget {
                         decoration: InputDecoration(
                             icon: const Icon(
                               Icons.vpn_key,
-                              color: Colors.green,
+                              color: Colors.black,
                             ),
                             focusedBorder: UnderlineInputBorder(
                                 borderSide:
@@ -146,9 +146,205 @@ class MainPage extends StatelessWidget {
                             borderRadius: BorderRadius.circular(20),
                             color: Colors.transparent,
                             child: InkWell(
+                              onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const SecondPage())),
                               borderRadius: BorderRadius.circular(20),
-                              splashColor: Colors.amber,
-                              onTap: () {},
+                              splashColor: Colors.black,
+                              
+                              child: const Center(
+                                child: Text(
+                                  "SIGN IN",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                              ),
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              gradient: const LinearGradient(
+                                  colors: [Colors.white, Colors.white],
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Text(
+                      "DON'T HAVE AN ACCOUNT ? ",
+                      style: TextStyle(
+                          fontSize: 11,
+                          color: Color.fromARGB(255, 65, 211, 255),
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      " SIGN UP",
+                      style: TextStyle(
+                          fontSize: 11,
+                          color: Color.fromARGB(255, 72, 255, 148),
+                          fontWeight: FontWeight.w700),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  double getSmallDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 2 / 3;
+  double getBiglDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 7 / 8;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFEEEEEE),
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            right: -getBiglDiameter(context) / 2,
+            bottom: -getBiglDiameter(context) / 2,
+            child: Container(
+              width: getSmallDiameter(context)+ getSmallDiameter(context),
+              height: getSmallDiameter(context)+ getSmallDiameter(context),
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                      colors: [Colors.blue, Colors.green],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter)),
+            ),
+          ),
+          Positioned(
+            left: -getBiglDiameter(context) / 4,
+            top: -getBiglDiameter(context) / 2.5,
+            child: Container(
+              child: const Center(
+                child: Text(
+                  "FileX",
+                  style: TextStyle(
+                      fontFamily: "Pacifico",
+                      fontSize: 70,
+                      color: Colors.white),
+                ),
+              ),
+              width: getBiglDiameter(context)+100,
+              height: getBiglDiameter(context)+150,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(colors: [
+                    Color.fromARGB(255, 0, 0, 0),
+                    Color.fromARGB(255, 0, 0, 0)
+                  ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+            ),
+          ),
+          Positioned(
+            right: -getBiglDiameter(context) -10000,
+            bottom: -getBiglDiameter(context) / 2,
+            child: Container(
+              width: getBiglDiameter(context)+300,
+              height: getBiglDiameter(context)+300,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle, color: Color.fromARGB(255, 242, 24, 133)),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      //border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10)),
+                  margin: const EdgeInsets.fromLTRB(20, 300, 20, 10),
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 25),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                            icon: const Icon(
+                              Icons.person,
+                              color: Colors.black,
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.grey.shade100)),
+                            labelText: "Name",
+                            enabledBorder: InputBorder.none,
+                            labelStyle: const TextStyle(color: Colors.grey)),
+                      ),
+                        TextField(
+                        decoration: InputDecoration(
+                            icon: const Icon(
+                              Icons.email,
+                              color: Colors.black,
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.grey.shade100)),
+                            labelText: "Email",
+                            enabledBorder: InputBorder.none,
+                            labelStyle: const TextStyle(color: Colors.grey)),
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            icon: const Icon(
+                              Icons.vpn_key,
+                              color: Colors.black,
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.grey.shade100)),
+                            labelText: "Password",
+                            enabledBorder: InputBorder.none,
+                            labelStyle: const TextStyle(color: Colors.grey)),
+                      )
+                    ],
+                  ),
+                ),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                        margin: const EdgeInsets.fromLTRB(0, 0, 20, 10),
+                        child: const Text(
+                          "FORGOT PASSWORD?",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontSize: 11),
+                        ))),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(20, 0, 20, 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width / 3,
+                        height: 40,
+                        child: Container(
+                          child: Material(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.transparent,
+                            child: InkWell(
+                              onTap: () => Navigator.pop(context),
+                              borderRadius: BorderRadius.circular(20),
+                              splashColor: Colors.black,
+                              
                               child: const Center(
                                 child: Text(
                                   "SIGN IN",
---
+++ lib/second.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  double getSmallDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 2 / 3;
+  double getBiglDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 7 / 8;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFEEEEEE),
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            right: -getBiglDiameter(context) / 2,
+            bottom: -getBiglDiameter(context) / 2,
+            child: Container(
+              width: getSmallDiameter(context)+ getSmallDiameter(context),
+              height: getSmallDiameter(context)+ getSmallDiameter(context),
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                      colors: [Colors.blue, Colors.green],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter)),
+            ),
+          ),
+          Positioned(
+            left: -getBiglDiameter(context) / 4,
+            top: -getBiglDiameter(context) / 2.5,
+            child: Container(
+              child: const Center(
+                child: Text(
+                  "FileX",
+                  style: TextStyle(
+                      fontFamily: "Pacifico",
+                      fontSize: 70,
+                      color: Colors.white),
+                ),
+              ),
+              width: getBiglDiameter(context)+100,
+              height: getBiglDiameter(context)+150,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(colors: [
+                    Color.fromARGB(255, 0, 0, 0),
+                    Color.fromARGB(255, 0, 0, 0)
+                  ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+            ),
+          ),
+          Positioned(
+            right: -getBiglDiameter(context) -10000,
+            bottom: -getBiglDiameter(context) / 2,
+            child: Container(
+              width: getBiglDiameter(context)+300,
+              height: getBiglDiameter(context)+300,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle, color: Color.fromARGB(255, 242, 24, 133)),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      //border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10)),
+                  margin: const EdgeInsets.fromLTRB(20, 300, 20, 10),
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 25),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                            icon: const Icon(
+                              Icons.email,
+                              color: Colors.blue,
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.grey.shade100)),
+                            labelText: "Email",
+                            enabledBorder: InputBorder.none,
+                            labelStyle: const TextStyle(color: Colors.grey)),
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            icon: const Icon(
+                              Icons.vpn_key,
+                              color: Colors.green,
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.grey.shade100)),
+                            labelText: "Password",
+                            enabledBorder: InputBorder.none,
+                            labelStyle: const TextStyle(color: Colors.grey)),
+                      )
+                    ],
+                  ),
+                ),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                        margin: const EdgeInsets.fromLTRB(0, 0, 20, 10),
+                        child: const Text(
+                          "FORGOT PASSWORD?",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontSize: 11),
+                        ))),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(20, 0, 20, 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width / 3,
+                        height: 40,
+                        child: Container(
+                          child: Material(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.transparent,
+                            child: InkWell(
+                              
+                              borderRadius: BorderRadius.circular(20),
+                              splashColor: Colors.black,
+                              onTap: () {},
+                              child: const Center(
+                                child: Text(
+                                  "SIGN IN",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                              ),
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              gradient: const LinearGradient(
+                                  colors: [Colors.white, Colors.white],
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Text(
+                      "DON'T HAVE AN ACCOUNT ? ",
+                      style: TextStyle(
+                          fontSize: 11,
+                          color: Color.fromARGB(255, 65, 211, 255),
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      " SIGN UP",
+                      style: TextStyle(
+                          fontSize: 11,
+                          color: Color.fromARGB(255, 72, 255, 148),
+                          fontWeight: FontWeight.w700),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
